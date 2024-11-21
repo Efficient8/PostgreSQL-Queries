056c9e5f62876356ac67eaf88914005b7c4b9af4
Efficient8 <efficient8ight@gmail.com>
2024-11-21 19:46:30 +0530

    Added Basic 20 to 40 Queries

--- Basic SQL Queries (Next 20).sql
+++ Basic SQL Queries (Next 20).sql
@@ -40,3 +40,80 @@ FROM Courses c
 LEFT JOIN Enrollments e ON c.courseid = e.courseid
 GROUP BY c.courseid, c.coursename
 ORDER BY c.coursename;
+
+-- 31. Group students by their department and count the number of students in each.
+SELECT * FROM Departments;
+SELECT * FROM Students;
+
+UPDATE Students SET DepartmentID=102 WHERE StudentID=2022202416;
+
+SELECT COUNT(StudentID), DepartmentID FROM Students
+GROUP BY DepartmentID;
+
+-- 32. Find the number of courses offered by each department.
+SELECT * FROM Courses;
+
+UPDATE Courses SET DepartmentID=102 WHERE courseid=6;
+
+SELECT DepartmentID, COUNT(coursename) FROM Courses
+GROUP BY DepartmentID;
+
+-- 33. Retrieve the total number of male and female students.
+SELECT * FROM Students;
+
+SELECT COUNT(gender), gender FROM Students
+GROUP BY gender;
+
+-- 34. Count the number of books authored by each author. 
+SELECT * FROM Library;
+
+UPDATE Library SET author='Thomas H. Cormen' WHERE bookid=4;
+
+SELECT COUNT(title), author FROM Library
+GROUP BY author;
+
+-- 35. List each grade along with the number of students who received it.
+SELECT * FROM Enrollments;
+
+SELECT COUNT(StudentID) CountOfStudents, grade FROM Enrollments
+GROUP BY grade;
+
+-- 36. Retrieve the names of students and the departments they belong to.
+SELECT * FROM Students;
+SELECT * FROM Departments;
+
+UPDATE Students SET departmentid=108 WHERE StudentID=2022202418;
+
+SELECT CONCAT(s.firstname, ' ', s.lastname), d.departmentname FROM Students s
+JOIN Departments d ON s.departmentid=d.departmentid;
+
+-- 37. List all courses along with their respective departments.
+SELECT * FROM Courses;
+
+SELECT c.coursename, d.departmentname FROM Courses c
+JOIN Departments d ON c.departmentid=d.departmentid;
+
+-- 38. Display the names of instructors and the courses they teach. 
+SELECT * FROM Instructors;
+SELECT * FROM Courses;
+
+SELECT CONCAT(i.firstname, ' ', i.lastname) as Instructor_Name, c.coursename FROM Instructors i
+JOIN Courses c ON i.departmentid=c.departmentid;
+
+-- 39. Retrieve the names of students and the books they have borrowed.
+SELECT * FROM Students;
+SELECT * FROM BookLoans;
+SELECT * FROM Library;
+
+SELECT CONCAT(s.firstname, ' ', s.lastname) Student_Name, l.title FROM Students s
+JOIN BookLoans b ON s.studentid=b.studentid
+JOIN Library l ON l.bookid=b.bookid;
+
+-- 40. List all students and the courses they are enrolled in.
+SELECT * FROM Students;
+SELECT * FROM Courses;
+SELECT * FROM Enrollments;
+
+SELECT CONCAT(s.firstname, ' ', s.lastname) Student_Name, c.coursename FROM Students s
+JOIN Enrollments e ON s.studentid=e.studentid
+JOIN Courses c ON c.courseid=e.courseid;
